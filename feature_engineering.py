edf918a26f2bda44b0436767e0eb8aa0950f3fc4
DavideBrex <davidebx96@gmail.com>
2019-05-07 00:41:37 +0200

    testing stuff

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,12 +1,24 @@
 #!/bin/bash
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.datasets import dump_svmlight_file
+
+def features_addition(data):
+    print("helo")
+    return data
+
+def features_remotion(data):
+    data = data.drop(["date_time", "gross_bookings_usd"], axis=1)
+    return data
+
 
 
 def main():
-    print("hello")
+    path="C://Users//david\Desktop//VU amsterdam//Data mining"
+    train = pd.read_csv(path+"/training_set_VU_DM.csv", nrows=1000000)
+    new_train = features_remotion(train)
+    new_train = features_addition(new_train)
 
 
 if __name__ == '__main__':
